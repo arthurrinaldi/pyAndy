1e21ed5edd64148a56e60c125c1d3997d5d2b0c6
mcsoini <martin.soini@unige.ch>
2019-01-25 16:04:35 +0100

    __init__ files

--- __init__.py
+++ __init__.py
@@ -6,8 +6,15 @@ Created on Sat Jan 19 14:30:02 2019
 @author: user
 """
 
+
+__all__ = ['core', 'auxiliary']
+
 from pyAndy.core.plotpage import PlotTiled
 from pyAndy.core.plotpage import PlotPage
 
 from pyAndy.core.plotpagedata import PlotPageData
+
+import pyAndy.core as core
+import pyAndy.auxiliary as auxiliary
+
 
