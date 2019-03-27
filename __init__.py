f81276c424ce702310867ee96fa9d8dcfab43f8e
mcsoini <martin.soini@unige.ch>
2019-03-27 13:50:21 +0100

    __init__: cleanup

--- __init__.py
+++ __init__.py
@@ -7,14 +7,10 @@ Created on Sat Jan 19 14:30:02 2019
 """
 
 
-__all__ = ['core', 'auxiliary']
-
 from pyAndy.core.plotpage import PlotTiled
 from pyAndy.core.plotpage import PlotPage
 
 from pyAndy.core.plotpagedata import PlotPageData
 
-import pyAndy.core as core
-import pyAndy.auxiliary as auxiliary
 
 
